b7a66c670dd8651df056ed704191412504ab9e0e
zherlock030 <zhouh@emnets.org>
2020-07-24 19:54:36 +0800

    clear comments

--- src _v4.cpp
+++ src _v4.cpp
@@ -1,5 +1,5 @@
 //yolov5s的调用
-//删除冗余注释，优化代码,
+//替换导入的网络，增加detect layer的功能。
 
 #include <memory>
 #include <torch/script.h>
@@ -24,7 +24,8 @@ using namespace torch::indexing;
 vector<string> split(const string& str, const string& delim) {
 	vector<string> res;
 	if("" == str) return res;
-	char * strs = new char[str.length() + 1] ;
+	//先将要切割的字符串从string类型转换为char*类型
+	char * strs = new char[str.length() + 1] ; //不要忘了
 	strcpy(strs, str.c_str());
 
 	char * d = new char[delim.length() + 1];
@@ -32,8 +33,8 @@ vector<string> split(const string& str, const string& delim) {
 
 	char *p = strtok(strs, d);
 	while(p) {
-		string s = p; 
-		res.push_back(s);
+		string s = p; //分割得到的字符串转换为string类型
+		res.push_back(s); //存入结果数组
 		p = strtok(NULL, d);
 	}
 	return res;
@@ -190,14 +191,28 @@ cv::Mat letterbox(Mat img, int new_height = 640, int new_width = 640, Scalar col
 
 
 at::Tensor make_grid(int nx = 20, int ny = 20){
+//yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+//        return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
+
   std::vector<at::Tensor> temp = torch::meshgrid({torch::arange(ny), torch::arange(nx)});
   at::Tensor yv = temp[0];
   at::Tensor xv = temp[1];
+  //cout << "line 202, " << temp[0].sizes() <<endl;
   at::Tensor retu = torch::stack({xv, yv},2).view({1,1,ny,nx,2}).toType(torch::kFloat);
+  //cout << "line 202, " << retu.sizes() <<endl;
+  //cout << "line 202, retu is " << retu.index({0,0,0,0,0}) <<endl;
+  //return torch::ones_like(retu);
   return retu;
 }
 
 at::Tensor clip_coords(at::Tensor boxes, auto img_shape){
+  /*
+  # Clip bounding xyxy bounding boxes to image shape (height, width)
+    boxes[:, 0].clamp_(0, img_shape[1])  # x1
+    boxes[:, 1].clamp_(0, img_shape[0])  # y1
+    boxes[:, 2].clamp_(0, img_shape[1])  # x2
+    boxes[:, 3].clamp_(0, img_shape[0])  # y2
+  */
   boxes.index({Slice(), 0}).clamp_(0, img_shape[1]);
   boxes.index({Slice(), 1}).clamp_(0, img_shape[0]);
   boxes.index({Slice(), 2}).clamp_(0, img_shape[1]);
@@ -217,10 +232,20 @@ float max_shape(int shape[], int len){
 
 
 at::Tensor scale_coords(int img1_shape[], at::Tensor coords, int img0_shape[]){
+//gain = max(img1_shape) / max(img0_shape)  # gain  = old / new
+//pad = (img1_shape[1] - img0_shape[1] * gain) / 2, (img1_shape[0] - img0_shape[0] * gain) / 2  # wh padding
+  cout << "line 240, im1 " << img1_shape << endl;
+  cout << "line 241, im1 is " << max_shape(img1_shape, 2) << " and im0 is " << max_shape(img0_shape, 3) << endl;
   float gain = max_shape(img1_shape, 2) / max_shape(img0_shape, 3);
   float padw = (img1_shape[1] - img0_shape[1] * gain ) / 2.0;
   float padh = (img1_shape[0] - img0_shape[0] * gain ) / 2.0;
-
+  cout << "line 244, gain is " << gain << ", padw is " << padw << " and padh is " << padh << endl;
+/*
+coords[:, [0, 2]] -= pad[0]  # x padding
+    coords[:, [1, 3]] -= pad[1]  # y padding
+    coords[:, :4] /= gain
+    clip_coords(coords, img0_shape)
+*/
   coords.index_put_({Slice(), 0}, coords.index({Slice(), 0}) - padw);
   coords.index_put_({Slice(), 2}, coords.index({Slice(), 2}) - padw);
   coords.index_put_({Slice(), 1}, coords.index({Slice(), 1}) - padh);
@@ -232,9 +257,12 @@ at::Tensor scale_coords(int img1_shape[], at::Tensor coords, int img0_shape[]){
 
 int main(int argc,char * argv[]){
 
+  //at::init_num_threads();//***zh,好像加上这句，速度有一点点提升
+
   string modelpath = "/home/zherlock/c++ example/object detection/files/yolov5s.torchscript";
   long start = time_in_ms();
   torch::jit::script::Module model = torch::jit::load(modelpath);
+  //long end = ;
   cout << "it took " << time_in_ms() - start << " ms to load the model" << endl;
   torch::jit::getProfilingMode() = false;
   torch::jit::getExecutorMode() = false;
@@ -246,18 +274,24 @@ int main(int argc,char * argv[]){
   Mat img = imread(img_path);
   Mat im0 = imread(img_path);
   auto tim0 = torch::from_blob(img.data, {img.rows, img.cols, img.channels()});
+  //cout << "line 269, tim0 shape is " << tim0.sizes() << endl;//(480,640,3)
+  //return 0;
   img = letterbox(img);//zh,,resize
+  
   cvtColor(img, img, CV_BGR2RGB);//***zh, bgr->rgb
   start = time_in_ms();
   img.convertTo(img, CV_32FC3, 1.0f / 255.0f);//zh, 1/255
   auto tensor_img = torch::from_blob(img.data, {img.rows, img.cols, img.channels()});
   tensor_img = tensor_img.permute({2, 0, 1});
   tensor_img = tensor_img.unsqueeze(0);
+  cout << "line 201, " << tensor_img.sizes() << endl;
   std::vector<torch::jit::IValue> inputs;
   inputs.push_back(tensor_img);
+  //end = ;
   cout << "it took " << time_in_ms() - start << " ms to preprocess image" << endl;
   start = time_in_ms();
   torch::jit::IValue output = model.forward(inputs);
+  //end = time_in_ms();
   cout << "it took " << time_in_ms() - start << " ms to model forward" << endl;
 
 
@@ -265,14 +299,11 @@ int main(int argc,char * argv[]){
   //set parameters
   int ny, nx;
   int na = 3;int no = 85;int bs = 1;
-  at::Tensor op;
+  at::Tensor op_1, op_2, op_3, op;
   at::Tensor y_0, y_1, y_2, y;
   at::Tensor grid_1, grid_2, grid_3, grid;
   float stride[3] = {8.0, 16.0, 32.0};
   at::Tensor anchor_grid = torch::ones({3, 1, 3, 1, 1, 2});
-
-
-  //read anchor_grid
   ifstream f;
   string gridpath = "/home/zherlock/c++ example/object detection/files/anchor_grid.txt";
   f.open(gridpath);
@@ -282,21 +313,59 @@ int main(int argc,char * argv[]){
     anchor_grid.index_put_({stoi(mp[0]),stoi(mp[1]),stoi(mp[2]),stoi(mp[3]),stoi(mp[4]),stoi(mp[5])}, torch::ones({1}) * stof(mp[6]) );
   }
   f.close();
+
+  //cout << "line 261, anchor grid is " << anchor_grid <<endl;
+
+
+//at::Tensor a = torch::ones({1}) * 18.0;
+//cout << "line 261, a is " << a <<endl;
+  //cout << "line 225, stride, " << stride[0] <<endl;
+  //grid_1 = torch::zeros({1});grid_2 = torch::zeros({1});grid_3 = torch::zeros({1});
+  //grid = torch::cat({grid_1, grid_2, grid_3}, 0);
+  //cout << "line 220, " << grid_1 << endl;
+
+  //at::Tensor xx = output.toList().get(0).toTensor();
+  //cout << "line 270, " << xx.sizes() << endl;
+  //cout << "line 271, " << xx.index({0,0,0,0,Slice(None, 5)}) << endl; //it fits
+
   
   //run
   for(int i = 0; i < 3; i++){
     op = output.toList().get(i).toTensor().contiguous();
+    //op = op.view({1, 255, op.sizes()[2],op.sizes()[3]});
+    //cout << "line 286, shape is " << op.sizes() << endl;
+    //cout << "line 287, test num is " << op.index({0,0,0,0,Slice(None, 5)}) << endl;
+    //cout << "line 287, test num is " << op.index({0,Slice(None, 5),0,0}) << endl;
+    //return 0;
     bs = op.sizes()[0]; ny = op.sizes()[2]; nx = op.sizes()[3];
     op = op.view({bs, na, no, ny, nx}).permute({0, 1, 3, 4, 2}).contiguous();
     grid = make_grid(nx, ny);
+    //cout << "line 290, op shapes is " << op.sizes() << endl;
+    //cout << "line 291, test num is " << grid.index({0,0,47,0,Slice(None, 5)}) << endl;
+    //return 0;
     y = op.sigmoid();
+    //cout << "line 292-0, test num is " << op.index({0,0,0,1,Slice(None, 5)}) << endl;
+    //cout << "line 292-1, test num is " << y.index({0,0,0,1,Slice(None, 5)}) << endl;
+    
+    //cout << "line 228, " << y.sizes() << endl;
+    //cout << "grid is " << grid.sizes() << endl;
+    //cout << "stride is " << stride[i] << endl;
+    //cout << "test num is " << grid.index({0,0,0,0,0}) << endl;
     at::Tensor test = y.index({Slice(),Slice(), Slice(),Slice(), Slice(0, 2)}) * 2.0 - 0.5;
     test = y.index({Slice(),Slice(), Slice(),Slice(), Slice(0, 2)}) * 2.0 - 0.5 + grid;
+    //cout << "line 304, test num is " << grid.index({0,0,0,0,Slice(None, 5)}) << endl;
+    //cout << "line 305, test num is " << test.index({0,0,0,0,Slice(None, 5)}) << endl;
     at::Tensor temp = (y.index({Slice(),Slice(), Slice(),Slice(), Slice(0, 2)}) * 2.0 - 0.5 + grid) * stride[i];
     y.index_put_({Slice(),Slice(), Slice(),Slice(), Slice(0, 2)}, temp);
+    //cout << "line 305, test num is " << temp.index({0,0,0,0,Slice(None, 5)}) << endl;
+
+
     temp = (2.0 * y.index({Slice(),Slice(), Slice(),Slice(), Slice(2, 4)})).pow(2) * anchor_grid.index({i});
+    //cout << "line 306, test num is " << temp.index({0,0,0,0,Slice(None, 5)}) << endl;
     y.index_put_({Slice(),Slice(), Slice(),Slice(), Slice(2, 4)}, temp);
     y = y.view({bs, -1, no});
+    //cout << "line 308-0, test num is " << y.sizes() << endl;
+    //cout << "line 309-1, test num is " << y.index({0,0,Slice(None, 5)}) << endl;
     if(i == 0){
       y_0 = y;
     } 
@@ -306,20 +375,34 @@ int main(int argc,char * argv[]){
     else{
       y_2 = y;
     }
+    //y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i].to(x[i].device)) * self.stride[i]  # xy
+    //y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+    //z.append(y.view(bs, -1, self.no))
 }
 
   op = torch::cat({y_0, y_1,y_2}, 1);
   op = op.view({-1, op.sizes()[2]});
+  //cout << "line 297, op shape " << op.sizes() << endl;
 
-  //read label information
+
+  cout << "line 306, test number is " << op.index({100,Slice(None, 5)}) << endl;
+  //return 0;
+  
+  
+
+
+  
+  //at::Tensor op = output.toTuple()->elements().at(0).toTensor();
+  //op = op.view({-1 ,op.sizes()[2] });
+  
+  
   std::vector<string> labels;
-  string labelpath = "/home/zherlock/c++ example/object detection/files/labels.txt";
+  string labelpath = "/home/zherlock/InstanceDetection/yolov5_old/labels.txt";
   f.open(labelpath);
   while (std::getline(f,str)){
     labels.push_back(str);
   }
   f.close();
-  
   start = time_in_ms();
   op = non_max_suppression(op, labels, 0.4, 0.5, true,  false);
   cout << "it took " << time_in_ms() - start << " ms to non_max_suppression" << endl;
@@ -331,14 +414,18 @@ int main(int argc,char * argv[]){
   }
   int img_shape[2] = {tensor_img.sizes()[2], tensor_img.sizes()[3]};
   int im0_shape[3] = {im0.rows, im0.cols, im0.channels()};
+  //cout << "line 422, img is " <<  img_shape[0] << " " << img_shape[1] << endl;
+  //cout << "line 422, im0 is " <<  im0_shape[0] << " " << im0_shape[1] << im0_shape[2] << endl;
+  //det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+  //cout << "line 423, " << tensor_img.sizes()(2,3) << endl;
   at::Tensor temp;
   temp = scale_coords(img_shape, op.index({Slice(), Slice(None, 4)}), im0_shape).round();
+  cout << "line 421, temp is " << temp << endl;
   op.index_put_({Slice(), Slice(None, 4)}, temp);
-
 
   for( int i = 0; i < op.sizes()[0]; i++ ){
 
-  cout << "start of object " << i+1 << endl;
+  cout << "start of object " << i << endl;
 
   at::Tensor opi = op.index({i});
   
@@ -357,8 +444,12 @@ int main(int argc,char * argv[]){
   cv::putText(im0,labels[op_class],Point(x1,y1),1,1.0,cv::Scalar(0,255,0),1);
   }
   
+
   imshow("test image", im0);
   waitKey();
+
+
+
 
   return 0;
 }
